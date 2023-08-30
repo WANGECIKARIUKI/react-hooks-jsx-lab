4c0a140f4bdde446b4ddeba16e93db5293dfab4b
LUCY <wangecikariu.luc@gmail.com>
2023-08-30 15:15:15 +0300

    JSX-Changed

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,13 +4,11 @@ import { name, city } from "../data/data.js";
 function Home() {
   // update the JSX being returned!
   
-  return (
-    <div id = "home">
-      <h1 style={{color: 'firebrick'}}>
-        {name} a Web Developer from {city}
-        </h1>
-    </div>
-  );
+ return (
+  <div id= "home">
+    <h1 style = {{color: "firebrick"}}> {name} is a Web Developer from {city}</h1>
+  </div>
+ )
 }
 
 export default Home;
